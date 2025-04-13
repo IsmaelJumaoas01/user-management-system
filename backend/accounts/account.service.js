4cddacc620c0387c971bea7eef8c2d01f33f1885
FitzFitzFitz69 <cutadalamo@gmail.com>
2025-04-13 08:01:59 +0800

    Implement role-based authorization, forgot password/reset password, and CRUD operations.

--- backend/accounts/account.service.js
+++ backend/accounts/account.service.js
@@ -5,11 +5,22 @@ const crypto = require('crypto');
 const sendEmail = require('_helpers/send-email');
 const db = require('_helpers/db');
 const Role = require('_helpers/role');
+const { Op } = require('sequelize');
 
 module.exports = {
     authenticate,
+    refreshToken,
+    revokeToken,
     register,
-    verifyEmail
+    verifyEmail,
+    forgotPassword,
+    validateResetToken,
+    resetPassword,
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
 };
 
 async function authenticate({ email, password, ipAddress }) {
@@ -133,4 +144,152 @@ async function sendAlreadyRegisteredEmail(email, origin) {
                <p>Your email <strong>${email}</strong> is already registered.</p>
                ${message}`
     });
+}
+
+async function forgotPassword({ email }, origin) {
+    const account = await db.Account.findOne({ where: { email } });
+  
+    if (!account) return;
+  
+    account.resetToken = randomTokenString();
+    account.resetTokenExpires = new Date(Date.now() + 24*60*60*1000);
+    await account.save();
+  
+    await sendPasswordResetEmail(account, origin);
+}
+
+async function validateResetToken({ token }) {
+    const account = await db.Account.findOne({
+      where: {
+        resetToken: token,
+        resetTokenExpires: { [Op.gt]: Date.now() }
+      }
+    });
+  
+    if (!account) throw 'Invalid token';
+  
+    return account;
+}
+  
+async function resetPassword({ token, password }) {
+    const account = await validateResetToken({ token });
+  
+    account.passwordHash = await hash(password);
+    account.passwordReset = Date.now();
+    account.resetToken = null;
+    await account.save();
+}
+
+async function getAll() {
+    const accounts = await db.Account.findAll();
+    return accounts.map(x => basicDetails(x));
+}
+  
+async function getById(id) {
+    const account = await getAccount(id);
+    return basicDetails(account);
+}
+  
+async function create(params) {
+
+    if (await db.Account.findOne({ where: { email: params.email } })) {
+      throw 'Email "' + params.email + '" is already registered';
+    }
+  
+    const account = new db.Account(params);
+    account.verified = Date.now();
+  
+
+    account.passwordHash = await hash(params.password);
+  
+    await account.save();
+  
+    return basicDetails(account);
+  }
+
+  async function update(id, params) {
+    const account = await getAccount(id);
+  
+
+    if (params.email && account.email !== params.email && await db.Account.findOne({ where: { email: params.email } })) {
+      throw 'Email "' + params.email + '" is already taken';
+    }
+  
+   
+    if (params.password) {
+      params.passwordHash = await hash(params.password);
+    }
+  
+    Object.assign(account, params);
+    account.updated = Date.now();
+    await account.save();
+  
+    return basicDetails(account);
+  }
+  
+  async function _delete(id) {
+    const account = await getAccount(id);
+    await account.destroy();
+  }
+
+
+
+async function getAccount(id) {
+    const account = await db.Account.findByPk(id);
+    if (!account) throw 'Account not found';
+    return account;
+  }
+
+
+async function refreshToken({ token, ipAddress }) {
+    const refreshToken = await getRefreshToken(token);
+    const account = await refreshToken.getAccount();
+  
+    const newRefreshToken = generateRefreshToken(account, ipAddress);
+    refreshToken.revoked = Date.now();
+    refreshToken.revokedByIp = ipAddress;
+    refreshToken.replacedByToken = newRefreshToken.token;
+    await refreshToken.save();
+    await newRefreshToken.save();
+  
+    const jwtToken = generateJwtToken(account);
+  
+    return {
+      ...basicDetails(account),
+      jwtToken,
+      refreshToken: newRefreshToken.token
+    };
+  }
+
+  async function revokeToken({ token, ipAddress }) {
+    const refreshToken = await getRefreshToken(token);
+  
+    refreshToken.revoked = Date.now();
+    refreshToken.revokedByIp = ipAddress;
+    await refreshToken.save();
+  }
+
+  async function sendPasswordResetEmail(account, origin) {
+    let message;
+    if (origin) {
+      const resetUrl = `${origin}/account/reset-password?token=${account.resetToken}`;
+      message = `<p>Please click the below link to reset your password, the link will be valid for 1 day:</p>
+                 <p><a href="${resetUrl}">${resetUrl}</a></p>`;
+    } else {
+      message = `<p>Please use the below token to reset your password with the <code>/account/reset-password</code> api route:</p>
+                 <p><code>${account.resetToken}</code></p>`;
+    }
+  
+    await sendEmail({
+      to: account.email,
+      subject: 'Sign-up Verification API - Reset Password',
+      html: `<h4>Reset Password Email</h4>
+             ${message}`
+    });
+  }
+
+async function getRefreshToken(token) {
+    const refreshToken = await db.RefreshToken.findOne({ where: { token } });
+    if (!refreshToken || !refreshToken.isActive) throw 'Invalid token';
+    return refreshToken;
 }
